587f560411546292610412cffe6c21c4f83eb4a1
Lachlon <lachlon@Lachlons-MacBook-Air.local>
2023-12-18 17:53:49 +1100

    test

--- navigation/screens/home.js
+++ navigation/screens/home.js
@@ -5,7 +5,7 @@ export default function HomeScreen({ navigation }) {
     return (
         <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text onPress={() => alert('This is the Home Screen!')}>
-                Home Screen! The best!
+                Home Screen!
             </Text>
 
         </View>
